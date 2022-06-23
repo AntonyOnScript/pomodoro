dc5bffae8a90d597d58af46988f7877f3e8d86ba
AntonyOnScript <santhiagoafk@gmail.com>
2022-06-23 10:19:59 -0300

    chore: add quasar

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { Quasar } from 'quasar'
+import '@quasar/extras/roboto-font-latin-ext/roboto-font-latin-ext.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.use(Quasar)
+app.mount('#app')
