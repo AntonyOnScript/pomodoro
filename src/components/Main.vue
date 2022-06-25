45eb903d4f1cd1194cf55b1a58fa89286db8b21f
AntonyOnScript <santhiagoafk@gmail.com>
2022-06-25 12:25:22 -0300

    feat: add lottie and make it looks better

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,54 +1,100 @@
 <script setup lang="ts">
 import Timer from './Timer.vue'
-import { reactive, computed } from 'vue'
+import { reactive, computed , ref } from 'vue'
 import { MainState } from '@/types'
+import CrazyApple from '@/assets/lottie/crazy-apple.json'
+
+interface ButtonState {
+    color: string,
+    text: string
+}
+
+const appleAnimation = ref()
 
 const state: MainState = reactive({
     start: false,
     itsColdTime: false
 })
 
-const buttonState = computed<String>(() => {
+const buttonState = computed<ButtonState>(() => {
     if (!state.start) {
-        return 'Start Pomodoro'
+        return {
+            color: 'primary',
+            text: 'Start Pomodoro'
+        }
     }
     if (state.itsColdTime) {
-        return 'Stop relax time (restart now!)'
+        return {
+            color: 'secondary',
+            text: 'Stop relax time (restart now!)'
+        }
     }
     
-    return 'Stop Pomodoro'
+    return {
+        color: 'negative',
+        text: 'Stop Pomodoro'
+    }
 })
 
 function handleChangeTimeColdness(): void {
+    if (!state.itsColdTime) {
+        appleAnimation.value.pause()
+    } else {
+        appleAnimation.value.play()
+    }
+
     state.itsColdTime = !state.itsColdTime
 }
 
 function handleButtonAction(): void {
     if (!state.start) {
+        appleAnimation.value.play()
         state.start = true
         return
     }
     if (state.itsColdTime) {
+        appleAnimation.value.play()
         state.start = !state.start
         state.itsColdTime = false
         state.start = !state.start
         return
     }
-
+    console.log(appleAnimation.value)
+    appleAnimation.value.pause()
     state.start = false
 }
 </script>
 
 <template>
-    <div>
+    <div
+        class="column items-center q-my-xl"
+    >
+        <lottie-animation
+            ref="appleAnimation"
+            :animationData="CrazyApple"
+            :loop="true"
+        />
         <Timer 
+            class="timer"
             :start="state.start"
             :itsColdTime="state.itsColdTime"
             @timeZero="handleChangeTimeColdness()" 
         />
-        <button @click="handleButtonAction()">{{ buttonState }}</button>
+        <q-btn 
+            class="q-mt-lg"
+            @click="handleButtonAction()"
+            push
+            :color="buttonState.color"
+        >
+            {{ buttonState.text }}
+        </q-btn>
     </div>
 </template>
 
 <style scoped>
+    .timer {
+        font-size: 2rem;
+        font-weight: 600;
+        text-transform: capitalize;
+    }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { Quasar } from 'quasar'
+import LottieAnimation from 'lottie-web-vue'
 import '@quasar/extras/roboto-font-latin-ext/roboto-font-latin-ext.css'
 import '@quasar/extras/material-icons/material-icons.css'
 import 'quasar/src/css/index.sass'
@@ -8,4 +9,5 @@ import 'quasar/src/css/index.sass'
 const app = createApp(App)
 
 app.use(Quasar)
+app.use(LottieAnimation)
 app.mount('#app')
